7935d493b6a89181f60d79385dbf5f61192afebf
Shmulik Cohen <anuk909@gmail.com>
2024-05-16 00:13:04 +0300

    format

--- dolos_main.py
+++ dolos_main.py
@@ -10,19 +10,20 @@ PLAIN_DIR = "raw_files"
 TEST_FILE = "human_eval_pile.jsonl"
 PROCESS_NUM = 16
 
+
 # itereate through folders in the zip folder
 def call_dolos(folder_name):
     print("working on folder: ", folder_name)
     start = time.time()
-    program_index = folder_name[len("problem_"):-len("_zipped")]
+    program_index = folder_name[len("problem_") : -len("_zipped")]
     program_results = []
-    
+
     folder_path = os.path.join(ZIP_DIR, folder_name)
-    
+
     for file in os.listdir(folder_path):
         # the order of the result is stored in the file's title
-        index = int(file[len("high_score_number_"):-len("_zipped.zip")])
-        
+        index = int(file[len("high_score_number_") : -len("_zipped.zip")])
+
         file_path = os.path.join(folder_path, file)
         stream = os.popen(f"dolos run -f terminal --language python {file_path}")
         output = stream.read()
@@ -30,24 +31,34 @@ def call_dolos(folder_name):
 
         for line in output:
             if "Similarity score:" in line:
-                score = float(line[len("Similarity score: "):])
+                score = float(line[len("Similarity score: ") :])
                 break
-        
+
         program_results.append({"high_score_number": index, "score": score})
 
-    sorted_program_results = sorted(program_results, key=lambda d: d["score"], reverse = True)
+    sorted_program_results = sorted(
+        program_results, key=lambda d: d["score"], reverse=True
+    )
 
     end = time.time()
-    run_time = end-start
+    run_time = end - start
     print("run time: ", run_time)
 
     result_dict = {
         "program_number": program_index,
         "sorted_program_results": sorted_program_results,
-        "time": run_time
+        "time": run_time,
     }
 
-    print("folder name: ", folder_name, "finished in ", run_time, " seconds with n = ", len(sorted_program_results), " results")
+    print(
+        "folder name: ",
+        folder_name,
+        "finished in ",
+        run_time,
+        " seconds with n = ",
+        len(sorted_program_results),
+        " results",
+    )
 
     # outputs the results to a file in the results folder
     output_path = "dolos_results"
@@ -59,8 +70,8 @@ def call_dolos(folder_name):
         with open(output_path, "x") as f:
             f.write(json.dumps(result_dict) + "\n")
 
-
     return (program_index, sorted_program_results, run_time)
+
 
 def zip_files():
     with open(TEST_FILE, "r") as f:
@@ -71,20 +82,21 @@ def zip_files():
         os.mkdir(PLAIN_DIR)
     except:
         print(f"{PLAIN_DIR} already exists")
-        
+
     try:
         os.mkdir(ZIP_DIR)
     except:
         print(f"{ZIP_DIR} already exists")
-        
-        
+
     # now we create the zipped files
     for i, result in enumerate(results):
-        start = time.time()    
-        gold_program = results[i]['test_str']   # since the gold program isn't stored in the file, we take it from the original dataset
-        
+        start = time.time()
+        gold_program = results[i][
+            "test_str"
+        ]  # since the gold program isn't stored in the file, we take it from the original dataset
+
         # make a folder to store the results in:
-        start_index = i+1
+        start_index = i + 1
         directory = f"Copyright_test_{start_index}"
         path = os.path.join(PLAIN_DIR, directory)
         try:
@@ -93,15 +105,14 @@ def zip_files():
             print("folder ", path, " already exists")
 
         # makes folder for zip files for Dolos
-        directory = f"problem_{start_index}_zipped"    
+        directory = f"problem_{start_index}_zipped"
         zip_path = os.path.join(ZIP_DIR, directory)
         try:
             os.mkdir(zip_path)
         except:
             print("folder ", zip_path, " already exists")
-        
 
-        for j, temp_result in enumerate(result['top_k']):
+        for j, temp_result in enumerate(result["top_k"]):
             # we only take the top 500 results
             if j >= 500:
                 break
@@ -128,13 +139,14 @@ def zip_files():
                 with open(output_path, "w") as f:
                     f.write(gold_program)
             except:
-                print("problem writing gold program")        
+                print("problem writing gold program")
 
             output_filename = os.path.join(zip_path, f"high_score_number_{j+1}_zipped")
-            shutil.make_archive(output_filename, 'zip', num_path)
-        
+            shutil.make_archive(output_filename, "zip", num_path)
+
         end = time.time()
         print("time taken: ", end - start)
+
 
 def main():
 
@@ -146,6 +158,7 @@ def main():
     with Pool(PROCESS_NUM) as p:
         results = p.map(call_dolos, folder_names)
         print(results)
+
 
 if __name__ == "__main__":
     main()
